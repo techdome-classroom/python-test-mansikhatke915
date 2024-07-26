d3b3a09ccada36c68da8bba04c7a9d0239b80677
MANSI KHATKE <mansikhatke915@gmail.com>
2024-07-26 13:31:45 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,21 @@
 class Solution:
-   
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+   def getTotalIsles(self, grid: list[list[str]]) -> int:
+    
+        def dfs(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 'W':
+                return
+            grid[r][c] = 'W'  # mark as visited
+            # visit all adjacent cells (up, down, left, right)
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        num_islands = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 'L':
+                    num_islands += 1
+                    dfs(r, c)
+        
+        return num_islands              
